7d266cf30e4e21ac2eaf076d20774ccc85256283
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-19 23:01:46 +0100

    CORRECTION

--- examples/clock.s
+++ examples/clock.s
@@ -3,8 +3,8 @@
 	loadi $59 %3 # Minutes.
 	loadi $42 %4 # Hours.
 	loadi $1 %5
-	loadi $59 %6
-	loadi $23 %7
+	loadi $58 %6
+	loadi $22 %7
 .print_hour
 	print %4 $2
 .print_min
@@ -13,10 +13,10 @@
 	print %2 $0
 .sec
 	add %5 %2 %2 # Add a second.
-	xor %2 %1 %2 # Check if there is a roll over. # sub %6 %2 %0
+	xor %2 %1 %9 # Check if there is a roll over. # sub %6 %2 %0
 	loadi $1 %8
-	add %8 %2 %2
-	add %6 %2 %0
+	add %8 %9 %9
+	add %6 %9 %0
 	jneg min
 	jmp print_sec # Update the screen and loop.
 .min
@@ -26,10 +26,10 @@
 	add %5 %3 %3
 	# Check if there is a roll over.
 	# sub %6 %2 %0
-	xor %2 %1 %2
+	xor %3 %1 %9
 	loadi $1 %8
-	add %8 %2 %2
-	add %6 %2 %0
+	add %8 %9 %9
+	add %6 %9 %0
 	jneg hour
 	# Update the screen and loop.
 	jmp print_min
@@ -40,10 +40,10 @@
 	add %5 %4 %4
 	# Check if there is a roll over.
 	# sub %7 %2 %0
-	xor %2 %1 %2
+	xor %4 %1 %9
 	loadi $1 %8
-	add %8 %2 %2
-	add %7 %2 %0
+	add %8 %9 %9
+	add %7 %9 %0
 	jneg halt
 	# Update the screen and loop.
 	jmp print_hour
