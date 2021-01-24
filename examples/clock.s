eb9f31322917a7625a4c14f8e42464b9ab1490ea
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-24 14:50:18 +0100

    new version

--- examples/clock.s
+++ examples/clock.s
@@ -14,8 +14,7 @@
 .sec
 	add %5 %2 %2 # Add a second.
 	xor %2 %1 %9 # Check if there is a roll over. # sub %6 %2 %0
-	loadi $1 %8
-	add %8 %9 %9
+	add %5 %9 %9
 	add %6 %9 %0
 	jneg min
 	jmp print_sec # Update the screen and loop.
@@ -23,8 +22,7 @@
 	loadi $0 %2# Reset the seconds.
 	add %5 %3 %3 # Add a minute.
 	xor %3 %1 %9 # Check if there is a roll over. # sub %6 %2 %0
-	loadi $1 %8
-	add %8 %9 %9
+	add %5 %9 %9
 	add %6 %9 %0
 	jneg hour
 	jmp print_min # Update the screen and loop.
@@ -32,8 +30,7 @@
 	loadi $0 %3 # Reset the minutes.
 	add %5 %4 %4 # Add an hour.
 	xor %4 %1 %9 # Check if there is a roll over. # sub %7 %2 %0
-	loadi $1 %8
-	add %8 %9 %9
+	add %5 %9 %9
 	add %7 %9 %0
 	jneg halt
 	jmp print_hour # Update the screen and loop.
--- linker.ml
+++ linker.ml
@@ -7,14 +7,14 @@ let label_map = Lmap.empty
 let rec add_labels liste ligne label_map = 
   match liste with 
   | [] -> label_map
-  | (LABEL s) :: q -> add_labels q (ligne + 1) (Lmap.add s ligne label_map)
+  | (LABEL s) :: q -> add_labels q ligne (Lmap.add s ligne label_map)
   | _ :: q -> add_labels q (ligne + 1) label_map
 ;;
 
 let rec transforms liste label_map =
   match liste with 
   | [] -> []
-  | (LABEL s) :: q -> L_ADD (0, 0, 0) :: (transforms q label_map)
+  | (LABEL s) :: q -> transforms q label_map
   | STORE (r, m) :: q -> L_STORE (r, m) :: (transforms q label_map)
   | LOAD (r, m) :: q -> L_LOAD (r, m) :: (transforms q label_map)
   | ADD (r1, r2, r3) :: q -> L_ADD (r1, r2, r3) :: (transforms q label_map)
